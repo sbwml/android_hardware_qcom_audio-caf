12d3dcd091ddd4e6d4d4d995e08db150d4e2322b
sbwml <984419930@qq.com>
2018-08-25 20:56:32 +0800

    first commit

---
+++ msm8998/voice_processing/Android.mk
@@ -0,0 +1,32 @@
+LOCAL_PATH:= $(call my-dir)
+
+# audio preprocessing wrapper
+include $(CLEAR_VARS)
+
+LOCAL_MODULE:= libqcomvoiceprocessing
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_RELATIVE_PATH := soundfx
+LOCAL_VENDOR_MODULE := true
+
+LOCAL_SRC_FILES:= \
+    voice_processing.c
+
+LOCAL_CFLAGS += \
+    -Wall \
+    -Werror \
+    -Wno-unused-function \
+    -Wno-unused-variable \
+
+LOCAL_C_INCLUDES += \
+    $(call include-path-for, audio-effects)
+
+LOCAL_SHARED_LIBRARIES := \
+    liblog \
+    libcutils
+
+LOCAL_SHARED_LIBRARIES += libdl
+
+LOCAL_CFLAGS += -fvisibility=hidden
+
+LOCAL_HEADER_LIBRARIES += libhardware_headers
+include $(BUILD_SHARED_LIBRARY)
